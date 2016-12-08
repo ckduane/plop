e006ec5e7ee34b051634eb365d784b0ba7decd7f
Matthew Dimesky & Ori Nachassi <pair+dimesky.onachassi@devbootcamp.com>
2016-12-08 10:35:41 -0800

    Updated restaurant and register model and views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,11 +3,11 @@ before_action :verifyLogin
 
   def index
     @r_workability = Restaurant.sort_by_workability.first(5)
-    @r_parking = Restaurant.sort_by_parking.first(5)
-    @r_wifi = Restaurant.sort_by_wifi.first(5)
-    @r_seating = Restaurant.sort_by_seating.first(5)
-    @r_outlet = Restaurant.sort_by_outlet.first(5)
-    @r_atmosphere = Restaurant.sort_by_atmosphere.first(5)
+    @r_parking = Restaurant.sort_by("parking_rating").first(5)
+    @r_wifi = Restaurant.sort_by("wifi_rating").first(5)
+    @r_seating = Restaurant.sort_by("seating_rating").first(5)
+    @r_outlet = Restaurant.sort_by("outlet_rating").first(5)
+    @r_atmosphere = Restaurant.sort_by("atmosphere_rating").first(5)
   end
 
   def search
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -6,69 +6,20 @@ class Restaurant < ActiveRecord::Base
   validates :address, presence: true
 
   def work_score #calculates workability score
-    if self.reviews.empty?
       work_score = 0
+    if self.reviews.empty?
+      work_score
     else
-    reviews = self.reviews
-    work_score = 0
-    reviews.each do |review|
-      work_score += review.work_score
-    end
-    work_score / reviews.length
+      reviews.reduce(0){|sum, review| sum + review.work_score} / reviews.length.to_f
     end
   end
 
-  def parking_score
-      parking_score = 0
-    if self.reviews.empty?
-      return parking_score
+  def score(rating) #must pass in rating in quotes
+      score = 0
+    if reviews.empty?
+      return score
     else
-      self.reviews.each{ |review| parking_score += review.parking_rating}
-      parking_score / reviews.length.to_f
-    end
-  end
-
-  def outlet_score
-    if self.reviews.empty?
-      outlet_score = 0
-    else
-      reviews = self.reviews
-      outlet_score = 0
-      reviews.each{ |review| outlet_score += review.outlet_rating}
-      outlet_score = outlet_score / reviews.length.to_f
-    end
-  end
-
-  def seating_score
-    if self.reviews.empty?
-      seating_score = 0
-    else
-      reviews = self.reviews
-      seating_score = 0
-      reviews.each{ |review| seating_score += review.seating_rating}
-      seating_score = seating_score / reviews.length.to_f
-    end
-  end
-
-  def atmosphere_score
-    if self.reviews.empty?
-      atmosphere_score = 0
-    else
-      reviews = self.reviews
-      atmosphere_score = 0
-      reviews.each{ |review| atmosphere_score += review.atmosphere_rating}
-      atmosphere_score = atmosphere_score / reviews.length.to_f
-    end
-  end
-
-  def wifi_score
-    if self.reviews.empty?
-      wifi_score = 0
-    else
-      reviews = self.reviews
-      wifi_score = 0
-      reviews.each{ |review| wifi_score += review.wifi_rating}
-      wifi_score = wifi_score / reviews.length.to_f
+      reviews.reduce(0){|sum, review| sum + review.send(rating)} / reviews.length.to_f
     end
   end
 
@@ -77,29 +28,9 @@ class Restaurant < ActiveRecord::Base
     restaurants.sort {|a, b| b.work_score <=> a.work_score}
   end
 
-  def self.sort_by_parking
+  def self.sort_by(rating) #must pass in rating in quotes
     restaurants = self.all
-    restaurants.sort {|a, b| b.parking_score <=> a.parking_score}
-  end
-
-  def self.sort_by_outlet
-    restaurants = self.all
-    restaurants.sort {|a, b| b.outlet_score <=> a.outlet_score}
-  end
-
-  def self.sort_by_seating
-    restaurants = self.all
-    restaurants.sort {|a, b| b.seating_score <=> a.seating_score}
-  end
-
-  def self.sort_by_atmosphere
-    restaurants = self.all
-    restaurants.sort {|a, b| b.atmosphere_score <=> a.atmosphere_score}
-  end
-
-  def self.sort_by_wifi
-    restaurants = self.all
-    restaurants.sort {|a, b| b.wifi_score <=> a.wifi_score}
+    restaurants.sort {|a, b| b.score(rating) <=> a.score(rating)}
   end
 
 end
