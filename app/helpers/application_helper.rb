48fe594679174cf28045bef53baaaa1588fcadb5
Matthew Dimesky & Ori Nachassi <pair+dimesky.onachassi@devbootcamp.com>
2016-12-08 11:52:31 -0800

    finished search bar feature

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def yelp_search(term)
+  client = Yelp.client.search('San Diego', { term: "#{term}" }, limit: 3)
+  client.businesses.each do |business|
+    Restaurant.create!(name: business.name,
+                        phone_number: business.phone,
+                        address: business.location.display_address.join(" "),
+                        link: business.url
+                        )
+    end
+  end
+
 end
