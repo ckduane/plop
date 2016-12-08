3e0ecfb3d14c7ccb336cea2b79e9b9ccd6a0908f
Christine Duane & Keith James <pair+ckduane.keithrjames@devbootcamp.com>
2016-12-08 15:37:42 -0800

    fixed merge conflicts;

--- form_for.rb
+++
@@ -1,7 +0,0 @@
-def form_for(stuff)
-  if stuff.is_a? ActiveRecord::Base
-    # make not nested route
-  else
-    "#{stuff[0].class.name.downcase}s/#{stuff[0].id}/#{stuff[1].class.name.downcase}s/#{stuff[1].id}"
-  end
-end
