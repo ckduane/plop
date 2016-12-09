6121105ffdb4d46cea8a52a5f1a4df7c8376ff8c
Matthew Dimesky & Ori Nachassi <pair+dimesky.onachassi@devbootcamp.com>
2016-12-09 15:16:35 -0800

    sped up processes

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,29 +7,31 @@ class Restaurant < ActiveRecord::Base
   validates :address, presence: true
 
   def work_score #calculates workability score
+    total_reviews = self.reviews.count
       work_score = 0
-    if self.reviews.empty?
+    if total_reviews == 0
       work_score
     else
-      reviews.reduce(0){|sum, review| sum + review.work_score} / reviews.length.to_f
+      reviews.reduce(0){|sum, review| sum + review.work_score} / total_reviews.to_f
     end
   end
 
   def score(rating) #must pass in rating in quotes
+    total_reviews = self.reviews.count
       score = 0
-    if reviews.empty?
+    if total_reviews == 0
       return score
     else
-      reviews.reduce(0){|sum, review| sum + review.send(rating)} / reviews.length.to_f
+      reviews.reduce(0){|sum, review| sum + review.send(rating)} / total_reviews.to_f
     end
   end
 
   def self.sort_by_workability(postal_code = "")
     if postal_code != ""
-      restaurants = Restaurant.where("address LIKE ?", "%#{postal_code}")
+      restaurants = Restaurant.where("address LIKE ?", "%#{postal_code}").includes(:reviews)
       return restaurants.sort {|a, b| b.work_score <=> a.work_score}
     else
-      restaurants = Restaurant.all
+      restaurants = Restaurant.all.includes(:reviews)
       return restaurants.sort {|a, b| b.work_score <=> a.work_score}
     end
   end
